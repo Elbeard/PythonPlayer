dcf94398472fbdcdac44914028f7e3c44011eda6
Semen Lapin <Elektrobeard@gmail.com>
2017-08-16 00:57:07 +0700

    Последняя версия плеера построенного на модулях Pyglet и Tkinter. Плеер будет переписан на других библиотеках.

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import tkinter
 import tkinter.filedialog
 from pyglet import media
-#если на компьтере отсутствуют прописанные выше библиотеки программа не заработает
-#надо их подгрузить в консоли набрать pip install (<название библиотеки>)
-#через переменную "createdMainWindow" можно отследить взаимодействие классов
 #программа пока играет только Wav файлы для mp3 надо устанавливать системную библиотеку AVbin
 
 class View:                                                 #класс отрисовки
     def __init__(self):
-        self.createdMainWindow = tkinter.Tk()               #создаём объект окна
-        self.drawWindow(self.createdMainWindow)             #запускаем рисование
+        self.drawWindow()             #запускаем рисование
 
-    def drawWindow(self, createdMainWindow):                #рисуем окно, и его элементы
-        self.createdMainWindow = createdMainWindow
+    def drawWindow(self):                #рисуем окно, и его элементы
+        self.createdMainWindow = tkinter.Tk()               #создаём объект окна
         self.createdMainWindow.config(height="50", width="300")        #конфигурируем главное окно
         self.createdMainWindow.title("Питониум")
         self.createdMainWindow.resizable(False, False)
-        self.frame1 = tkinter.Frame(createdMainWindow)      #делим окно на две невидимые рамки
-        self.frame2 = tkinter.Frame(createdMainWindow)
+        self.frame1 = tkinter.Frame(self.createdMainWindow)      #делим окно на две невидимые рамки
+        self.frame2 = tkinter.Frame(self.createdMainWindow)
         self.frame1.grid(row=0)
         self.frame2.grid(row=1)
         self.btnOpen = tkinter.Button(self.frame1, text="Open")             #рисуем кнопки
@@ -35,7 +31,7 @@ class View:                                                 #класс отри
 
 class Controller:                                       #класс управления
     def __init__(self):
-        self.view = View()                              #подключаем Viuw 
+        self.view = View()                              #подключаем Viuw
         self.player = media.Player()                    #укорачиваем вызов функции
         self.btnPlay = False                            #состояние кнопки Play/Pause
         #-----------------------обрабатываем событие нажатия на кнопки--------------------#
@@ -82,7 +78,6 @@ class Controller:                                       #класс управл
 
 
 controller = Controller()                               #создание экземпляра класса Controller
-controller.view.createdMainWindow.mainloop()                  #запуск основного цикла программы
+controller.view.createdMainWindow.mainloop()            #запуск основного цикла программы
 
 
-#!!!почитать про метод  insert!!!
